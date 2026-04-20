111e123e3029ae1f2273bf4db16d3acfb7766eed
ysd2160 <ysd2445@gmail.com>
2026-04-20 20:56:38 +0530

    fourth commit

--- src/utils/AxiosInstance.js
+++ src/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const authApi = axios.create({
-    baseURL: "https://socioinsta-backend.onrender.com//v1/api/auth",
+    baseURL: "https://socioinsta-backend.onrender.com/v1/api/auth",
     headers: {
         "Content-Type": "application/json"
     },
@@ -9,7 +9,7 @@ export const authApi = axios.create({
 })
 
 export const userApi = axios.create({
-    baseURL: "https://socioinsta-backend.onrender.com//v1/api/user",
+    baseURL: "https://socioinsta-backend.onrender.com/v1/api/user",
     withCredentials: true,
 
 })
@@ -20,6 +20,6 @@ export const postApi = axios.create({
 })
 
 export const commentApi = axios.create({
-    baseURL: "https://socioinsta-backend.onrender.com//v1/api/comment",
+    baseURL: "https://socioinsta-backend.onrender.com/v1/api/comment",
     withCredentials: true
 })
