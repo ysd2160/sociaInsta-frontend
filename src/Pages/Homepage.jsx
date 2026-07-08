f3b89d8e892356ff1f33ccd1eec40cc88cfde45f
ysd2160 <ysd2445@gmail.com>
2026-07-08 22:58:25 +0530

    homepage

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,46 +2,46 @@ import React, { useEffect } from 'react'
 import { authApi } from '../utils/AxiosInstance'
 import { useDispatch, useSelector } from 'react-redux'
 import { Logout } from '../Redux/authSlice'
+import { useNavigate } from 'react-router-dom'
+
 import SideBaar from '../Components/SideBaar'
 import Posts from '../Components/Posts'
 import SuggestedUser from '../Components/SuggestedUser'
 
 const Homepage = () => {
-     const user = useSelector(state=>state.auth)
-     const dispatch = useDispatch()
+    const user = useSelector((state) => state.auth.user) // apne state ke hisab se change karna
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
 
-
-     
-    const logoutHandler = async()=>{
-        try {
-            let response = await authApi.get("/logout")
-            console.log(response);
+    useEffect(() => {
+        if (!user) {
             dispatch(Logout())
+            navigate("/login")
+        }
+    }, [user, dispatch, navigate])
 
-            
+    const logoutHandler = async () => {
+        try {
+            await authApi.get("/logout")
+            dispatch(Logout())
+            navigate("/login")
         } catch (error) {
-            console.log(error);
-            
+            console.log(error)
         }
     }
-  return (
-    <>
-    <div className='flex  lg:gap-10 bg-slate-900 w-full min-h-screen '>
 
-<div className='lg:w-1/3'>
+    return (
+        <div className="flex lg:gap-10 bg-slate-900 w-full min-h-screen">
+            <div className="lg:w-1/3">
+                <SideBaar />
+            </div>
 
-  
-    <SideBaar/>
-</div>
-
-<div className='flex gap-10  '>
-
-    <Posts/>
-    <SuggestedUser/>
-</div>
-    </div>
-    </>
-  )
+            <div className="flex gap-10">
+                <Posts />
+                <SuggestedUser />
+            </div>
+        </div>
+    )
 }
 
 export default Homepage
